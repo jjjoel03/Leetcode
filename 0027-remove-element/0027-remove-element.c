7072ffa97f3af32f39b9d3c217b6104f31634ae9
jjjoel03 <joeljobinjames@gmail.com>
2026-08-27 20:12:58 +0530

    Time: 0 ms (100%), Space: 10.3 MB (69.54%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.c
@@ -0,0 +1,9 @@
+int removeElement(int* nums, int numsSize, int val) {
+    int i, k=0, temp;
+    for(i=0;i<numsSize;i++){
+        if(nums[i]!=val){
+            nums[k]=nums[i];
+            k++;
+    }}
+    return k;
+}
